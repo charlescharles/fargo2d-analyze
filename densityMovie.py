580ec070a9f65237f1cb8d8d3f6b9564a910a60f
charlescharles <platypode@gmail.com>
2014-11-17 20:44:49 -0500

    add density movie maker

---
+++ densityMovie.py
@@ -0,0 +1,72 @@
+__author__ = 'cguo'
+
+from fargoParser import FargoParser
+from fargoPlotter import FargoPlotter
+from optparse import OptionParser
+import fargoDiagnostics as fd
+import numpy as np
+import glob
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+class FargoMovieMaker:
+
+    def __init__(self, inputDir, outputDir, batchSize):
+        self.outputDir = outputDir
+
+        self.parser = FargoParser(inputDir, batchSize)
+
+        secondaryOrbit = np.loadtxt(inputDir + "/planet0.dat")
+        secondaryX = secondaryOrbit[:, 1]
+        secondaryY = secondaryOrbit[:, 2]
+
+        self.secondaryRadius = np.sqrt(np.add(np.square(secondaryX), np.square(secondaryY)))
+        self.secondaryTheta = np.arctan2(secondaryY, secondaryX)
+
+        params = self.parser.getParams()
+        self.params = params
+        self.outputDir = outputDir
+
+    def go(self):
+        dens, _, _ = self.parser.getNextBatch()
+
+        r, theta = np.meshgrid(self.params['radialIntervals'], self.params['thetaIntervals'])
+        plt.ioff()
+        #-- Plot... ------------------------------------------------
+        for i in range(100):
+            fig = plt.figure()
+            ax = plt.subplot(111, polar=True)
+            ax.contourf(theta, r, np.log(dens[i]).transpose())
+            ax.scatter([self.secondaryTheta[i]], [self.secondaryRadius[i]], s=150)
+            ax.set_rmax(1.5)
+            ax.set_title(r'$\theta_{sec}=' + "{0:.2f}".format(self.secondaryTheta[i] % 6.283) + r'$ rad', va='bottom')
+            plt.savefig(self.outputDir + "/figs/dens" + str(i) + ".png")
+            plt.close(fig)
+
+
+
+def main():
+    optParser = OptionParser()
+    optParser.add_option('-i', '--inputdirectory', action='store',
+                        type='string', dest='inputDirectory')
+
+    optParser.add_option('-b', '--batchsize', action='store',
+                         type='int', dest='batchSize', default=100)
+    
+    optParser.add_option('-o', '--outputdirectory', action='store',
+                         type='string', dest='outputDirectory')
+
+    optParser.add_option('-p', '--plotdirectory', action='store',
+                         type='string', dest='plotDirectory')
+
+    (options, args) = optParser.parse_args()
+
+    if not options.inputDirectory:
+        optParser.error('you must specify an input directory with -i or --inputdirectory')
+
+    movies = FargoMovieMaker(options.inputDirectory, options.outputDirectory, options.batchSize)
+    movies.go()
+
+if __name__ == '__main__':
+    main()
