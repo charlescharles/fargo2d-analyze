66c271a1f7777fe01384c58bf8ef37b9bb52a301
charlescharles <platypode@gmail.com>
2015-05-11 23:34:00 -0400

    wip

--- tqAnalysis.py
+++ tqAnalysis.py
@@ -27,17 +27,17 @@ def azimuthalStack(a, ns):
 
 
 """
-calculate torque T(r) for specified azimuthal modes.
+calculate torque density dT/dr(r) for specified azimuthal modes.
 parameters (dens, r_sup, r_inf, r_med, theta) have shape (nr, ns)
 `modes` has shape (n_modes)
 returns array of shape (len(modes), nr)
 """
-def computeTorque(mb, secr, sect, dens, r_sup, r_inf, r_med, theta, modes, indirect_term):
+def computeTorqueDensity(mb, secr, sect, dens, r_med, theta, modes, indirect_term):
     nr, ns = r_med.shape
     n_modes = len(modes)
     psi = theta - sect
 
-    area = np.pi * (np.square(r_sup) - np.square(r_inf)) / ns
+    length = 2. * np.pi * r_med / ns
 
     scale = dens * mb * secr * r_med * np.sin(psi)
 
@@ -46,8 +46,8 @@ def computeTorque(mb, secr, sect, dens, r_sup, r_inf, r_med, theta, modes, indir
     if indirect_term:
         force -= 1. / np.power(np.square(r_med) + np.square(secr) - 2. * r_med * secr * np.cos(psi), 1.5)
 
-    # torque per cell, shape (nr, ns)
-    cell_tq = area * scale * force
+    # dT/dr per cell. shape (nr, ns)
+    cell_tq = length * scale * force
 
     # tile cell_tq along z axis n_modes times
     # resulting array has shape (n_modes, nr, ns)
@@ -94,6 +94,7 @@ def main():
 
     radIntervals = np.multiply((2.0 / 3.0), ((cube(r_sup) - cube(r_inf)) / (square(r_sup) - square(r_inf))))
     thetaIntervals = np.linspace(0, 2*np.pi, 574)
+    dr = np.ediff1d(radialEdges)
 
     r_med = azimuthalStack(radIntervals, ns)
     r_sup = azimuthalStack(r_sup, ns)
@@ -104,7 +105,7 @@ def main():
     secr, sectheta = getTrajectory()
 
     i = 0
-    tqFourier = []
+    tqDensityFourier = []
     totalTqDirect = []
     angularMomentum = []
     while True:
@@ -115,8 +116,9 @@ def main():
             print 'finished at ' + str(i)
             break
 
-        tqFourier.append(computeTorque(mb, secr[i], sectheta[i], dens, r_sup, r_inf, r_med, theta, np.arange(11), True))
-        totalTqDirect.append(np.sum(computeTorque(mb, secr[i], sectheta[i], dens, r_sup, r_inf, r_med, theta, [0], False)))
+        tqDensityFourier.append(computeTorqueDensity(mb, secr[i], sectheta[i], dens, r_med, theta, np.arange(11), True))
+        directDens = computeTorqueDensity(mb, secr[i], sectheta[i], dens, r_med, theta, [0], False)[0]
+        totalTqDirect.append(np.sum(directDens * dr))
         angularMomentum.append(computeL(dens, vtheta, r_sup, r_inf, r_med))
 
         if i%100 == 0:
@@ -124,7 +126,7 @@ def main():
         i += 1
 
     print 'saving'
-    np.save('parsedDiagnostics/tqFourier', tqFourier)
+    np.save('parsedDiagnostics/tqFourier', tqDensityFourier)
     np.save('parsedDiagnostics/tqDirect', totalTqDirect)
     np.save('parsedDiagnostics/angularMomentum', angularMomentum)
 
